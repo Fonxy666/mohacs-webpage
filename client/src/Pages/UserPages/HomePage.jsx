31cc505bd05b62bc9abe73cd1ff72f093e199eca
fonxy6 <viktor_6@windowslive.com>
2024-01-17 12:55:42 +0100

    do Homepage

--- client/src/Pages/UserPages/HomePage.jsx
+++ client/src/Pages/UserPages/HomePage.jsx
@@ -1,44 +1,86 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../Components/Navbar";
 import AcePokerShopImage from "../../Images/ace_poker_shop.jpg";
 import JumboPokerShopImage from "../../Images/jumbo_poker_shop.jpg";
 import Logo from "../../Images/logo2.png";
-import { ImageContainer, ShopImage, LogoContainer, StyledLogo, CurvedTextContainer } from "../../Styles/HomePage.Styled";
-import ReactCurvedText from 'react-curved-text';
+import { HomePageContainer, StyledLogo, LogoText, ShopImage, ShopText } from "../../Styles/HomePage.Styled";
 
 const HomePage = () => {
+    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  return (
-    <div>
-        <Navbar/>
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowWidth(window.innerWidth);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, [window.innerWidth]);
+
+    return (
         <div>
-            <div style={{ margin: "auto", width: "50%" }}>
-                <LogoContainer>
-                    <StyledLogo src={Logo} alt="image"/>
-                </LogoContainer>
-                <CurvedTextContainer>
-                    <ReactCurvedText
-                        width={500}
-                        height={500}
-                        cx={200}
-                        cy={200}
-                        rx={200}
-                        ry={200}
-                        startOffset={0}
-                        reversed={false}
-                        text=" Üdvözlünk Mohács Ász & Jumbó Póker ruhabolt oldalán!"
-                        textProps={{ style: { fontSize: 25, fill: "white" } }}/>
-                </CurvedTextContainer>
-            </div>
-                <ImageContainer>
-                    <ShopImage src={AcePokerShopImage} alt="image"/>
-                </ImageContainer>
-                <ImageContainer>
-                    <ShopImage src={JumboPokerShopImage} alt="image"/>
-                </ImageContainer>
+            {windowWidth > 1200 ? (
+                <div>
+                    <Navbar/>
+                    <HomePageContainer>
+                        <div className="text-center">
+                            <ShopText>
+                                Üdvözöljük az Ász Póker butikban, ahol exkluzív és stílusos ruhákat kínálunk férfiaknak és nőknek.
+                                Minőségi anyagokból készült kollekciónkban garantáltan megtalálhatja az egyedi stílusokat.
+                                Fedezze fel az Ász Póker divatvilágát, ahol kifinomultság és kényelem találkozik!
+                            </ShopText>
+                            <ShopImage src={AcePokerShopImage}/>
+                        </div>
+                        <div className="text-center">
+                            <LogoText>
+                                Üdvözlünk Mohács Ász & Jumbó Póker ruhabolt oldalán!
+                            </LogoText>
+                            <StyledLogo src={Logo} alt="image"/>
+                        </div>
+                        <div className="text-center">
+                            <ShopText>
+                                Üdvözöljük az Jumbó Póker butikban, ahol exkluzív és stílusos ruhákat kínálunk nőknek és gyermekeknek.
+                                Minőségi anyagokból készült kollekciónkban garantáltan megtalálhatja az egyedi stílusokat.
+                                Fedezze fel az Jumbó Póker divatvilágát, ahol kifinomultság és kényelem találkozik!
+                            </ShopText>
+                            <ShopImage src={JumboPokerShopImage}/>
+                        </div>
+                    </HomePageContainer>
+                </div>
+            ) : (
+                <div>
+                    <Navbar/>
+                    <HomePageContainer>
+                        <div className="text-center">
+                            <LogoText>
+                                Üdvözlünk Mohács Ász & Jumbó Póker ruhabolt oldalán!
+                            </LogoText>
+                            <StyledLogo src={Logo} alt="image"/>
+                        </div>
+                        <div className="text-center">
+                            <ShopImage src={AcePokerShopImage}/>
+                            <ShopText>
+                                Üdvözöljük az Ász Póker butikban, ahol exkluzív és stílusos ruhákat kínálunk férfiaknak és nőknek.
+                                Minőségi anyagokból készült kollekciónkban garantáltan megtalálhatja az egyedi stílusokat.
+                                Fedezze fel az Ász Póker divatvilágát, ahol kifinomultság és kényelem találkozik!
+                            </ShopText>
+                        </div>
+                        <div className="text-center">
+                            <ShopImage src={JumboPokerShopImage}/>
+                            <ShopText>
+                                Üdvözöljük az Jumbó Póker butikban, ahol exkluzív és stílusos ruhákat kínálunk nőknek és gyermekeknek.
+                                Minőségi anyagokból készült kollekciónkban garantáltan megtalálhatja az egyedi stílusokat.
+                                Fedezze fel az Jumbó Póker divatvilágát, ahol kifinomultság és kényelem találkozik!
+                            </ShopText>
+                        </div>
+                    </HomePageContainer>
+                </div>
+            )}
         </div>
-    </div>
-  );
+    );
 };
 
 export default HomePage;
--- client/src/Styles/HomePage.Styled.js
+++ client/src/Styles/HomePage.Styled.js
@@ -1,39 +1,75 @@
 import styled from "styled-components";
 
-export const LogoContainer = styled.div`
-    width: 30%;
-    margin: 5vh auto 0px auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
+export const HomePageContainer = styled.div`
+    @media only screen and (min-width: 1200px) {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: 1fr;
+        grid-column-gap: 0px;
+        grid-row-gap: 0px;
+    };
 `;
-
-export const CurvedTextContainer = styled.div`
-    margin-top: 10%;
-    width: 30%;
-    margin: auto;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-`
 
 export const StyledLogo = styled.img`
-    width: 100%;
-    height: 100%;
-    border-radius: 50px;
+    border: 3px solid black;
+    border-radius: 15px;
+    @media only screen and (min-width: 1200px) {
+        grid-area: 1 / 2 / 2 / 3;
+        width: 60%;
+    };
+    @media only screen and (max-width: 1200px) {
+        
+    }; 
+    @media only screen and (min-width: 700px) and (max-width: 1200px) {
+        width: 35%;
+        margin: 10% 0% 10% 0%;
+    };
+    @media only screen and (max-width: 700px) {
+        width: 50%;
+        margin: 10% 0% 10% 0%;
+    };
 `;
 
-export const ImageContainer = styled.div`
-    width: 20%;
-    height: 20%;
-    object-fit: contain;
+export const LogoText = styled.div`
+    color: white;
+    font-style: oblique;
+    @media only screen and (min-width: 1200px) {
+        margin-top: 20%;
+        margin-bottom: 10%;
+        font-size: 40px;
+    };
+    @media only screen and (max-width: 1200px) {
+        margin: 10% 5% 0% 5%;
+        font-size: 30px;
+    };   
 `;
 
 export const ShopImage = styled.img`
-    width: 100%;
-    height: 100%;
+    border: 3px solid white;
+    border-radius: 15px;
+    @media only screen and (min-width: 1200px) {
+        grid-area: 1 / 3 / 2 / 4;
+        width: 60%;
+        margin: 0% 20% 0% 20%;
+    };
+    @media only screen and (min-width: 700px) and (max-width: 1200px) {
+        width: 30%;
+    };
+    @media only screen and (max-width: 700px) {
+        width: 45%;
+    };
 `;
 
-export const StyledCurvedText = styled.text`
-    fill: white;
+export const ShopText = styled.div`
+    color: white;
+    font-style: oblique;
+    @media only screen and (min-width: 1200px) {
+        margin: 20vh 20% 5vh 20%;
+        font-size: 20px;
+    };
+    @media only screen and (max-width: 1200px) {
+        font-size: 16px;
+        margin: 10% 20% 0% 20%;
+        padding-bottom: 10%;
+    };
 `;
